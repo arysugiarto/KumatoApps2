9857af4126f4c809f91ae6b1767c57489e329624
Ary Sugiarto <arysugiarto10@gmail.com>
2018-12-24 10:38:29 +0700

    selebrasi toolbar solved

--- app/src/main/java/com/example/arysugiarto/kumatoapps/FormAddUpdateActivity.java
+++ app/src/main/java/com/example/arysugiarto/kumatoapps/FormAddUpdateActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -58,6 +59,8 @@ public class FormAddUpdateActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_add_update);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
 
         tvOneTimeDate = (TextView) findViewById(R.id.tv_one_time_alarm_date);
         tvOneTimeTime = (TextView) findViewById(R.id.tv_one_time_alarm_time);
